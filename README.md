ec99d08a9d2b8357b09a10a5a5eaeac67e4411f1
Escobar, Emilio <emilio_escobar@epam.com>
2022-08-17 22:48:49 -0500

    HW6 - Test and Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Hi, Task 6 is still work in progress.
+# Task 6 - SQS and SNS Async Microservices Communication
+## All tasks has been implemented. Estimate score : 6 Points
--- products-service/src/functions/catalogBatchProcess/__test__/handler.spec.ts
+++ products-service/src/functions/catalogBatchProcess/__test__/handler.spec.ts
@@ -1,11 +1,10 @@
 import { catalogBatchProcess } from "../handler";
 import * as productService from "src/services/ProductService";
-import * as snsService from "src/services/SNSService";
 import * as middyfy from "@libs/lambda";
 
 jest.mock("@libs/lambda");
 jest.mock("src/services/ProductService");
-jest.mock("src/services/SNSService");
+
 const stringifyProduct= '{"\ufeffcount":"5","description":"Test2","price":"20","title":"Test2"}';
 const mockEvent = {
   Records: [
@@ -41,31 +40,22 @@ describe("getProducts test", () => {
   });
   it("it should trigger one sqs message", async () => {
     let publishMessage = jest.spyOn(
-      snsService,
-      "publishSNSMessage"
+      productService,
+      "publishProductsViaSNS"
     );
     publishMessage.mockImplementation(() => null);
     await catalogBatchProcess(mockEvent);
     expect(publishMessage.mock.calls.length).toBe(1);
   });
-  it("title should be Products uploaded", async () => {
+  it("content should be the product mapped and stringified", async () => {
     let publishMessage = jest.spyOn(
-      snsService,
-      "publishSNSMessage"
-    );
-    publishMessage.mockImplementation(() => null);
-    await catalogBatchProcess(mockEvent);
-    expect(publishMessage.mock.calls[0][0]).toBe('Products uploaded');
-  });
-  it("title should be the product mapped and stringified uploaded", async () => {
-    let publishMessage = jest.spyOn(
-      snsService,
-      "publishSNSMessage"
+      productService,
+      "publishProductsViaSNS"
     );
     publishMessage.mockImplementation(() => null);
     await catalogBatchProcess(mockEvent);
     const expectedProduct={count:5, description:'Test2', price:20, title:'Test2'};
-    const returnedProduct=JSON.parse(publishMessage.mock.calls[0][1])[0];
+    const returnedProduct=publishMessage.mock.calls[0][0][0];
     expect(returnedProduct.count).toBe(expectedProduct.count);
     expect(returnedProduct.description).toBe(expectedProduct.description);
     expect(returnedProduct.price).toBe(expectedProduct.price);
